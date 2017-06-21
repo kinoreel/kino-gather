bfad2743cc46ba71e6b76270c0fa41249c695fc8
unruffled-nightingale <unruffled.nightingale@gmail.com>
2017-06-21 19:17:21 +0100

    Changed bad file names

--- sql/tables/test.sql
+++
@@ -1,7 +0,0 @@
-create table test(
-   id INT PRIMARY KEY     NOT NULL,
-   name           TEXT    NOT NULL,
-   age            INT     NOT NULL,
-   address        CHAR(50),
-   salary         REAL
-);
