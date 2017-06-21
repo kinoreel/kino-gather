e3b9cbf19e84fa20ea465702fdc32793ac011493
unruffled-nightingale <unruffled.nightingale@gmail.com>
2017-06-21 23:13:35 +0100

    Added schema reference to columns

--- sql/views/v_movies.sql
+++ sql/views/v_movies.sql
@@ -1,10 +1,10 @@
 create or replace view gather.v_movies as
-select imdb_id
-	  , title
-      , runtime
-      , rated
-      , released
-  from gather.omdb_main x
-  left join kino.movies y
+select x.imdb_id
+      , x.title
+      , x.runtime
+      , x.rated
+      , x.released
+    from gather.omdb_main x
+    left join kino.movies y
     on x.imdb_id = y.imdb_id
- where y.imdb_id is null;
+    where y.imdb_id is null;
