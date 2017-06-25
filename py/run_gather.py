e1715da595489ac11391b3008b3ee99bc0f85a6a
unruffled-nightingale <unruffled.nightingale@gmail.com>
2017-06-25 23:31:38 +0100

    Added awards, and run script for gather2kino function

--- py/run_gather.py
+++ py/run_gather.py
@@ -2,15 +2,15 @@ import sys
 
 from gather import Gather
 
-server = sys.argv[0]
-port = sys.argv[1]
-database = sys.argv[2]
-username = sys.argv[3]
-password = sys.argv[4]
-omdb_key = sys.argv[5]
-tmdb_key = sys.argv[6]
-guidebox_key = sys.argv[7]
-youtube_films_key = sys.argv[8]
+server = sys.argv[1]
+port = sys.argv[2]
+database = sys.argv[3]
+username = sys.argv[4]
+password = sys.argv[5]
+omdb_key = sys.argv[6]
+tmdb_key = sys.argv[7]
+guidebox_key = sys.argv[8]
+youtube_films_key = sys.argv[9]
 
 get = Gather(server, port, database, username, password, omdb_key, tmdb_key, guidebox_key, youtube_films_key)
 
