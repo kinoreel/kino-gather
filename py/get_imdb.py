ab667f09695a1d331bc4f0b1c6f8906de6e4e169
Ted Johansson <tedjohanssondeveloper@gmail.com>
2017-07-01 14:29:58 +0100

    Simple script to get data into kafka

---
+++ py/get_imdb.py
@@ -0,0 +1,16 @@
+import json
+import sys
+
+from kafka import KafkaProducer
+
+try:
+    from GLOBALS import KAFKA_BROKER
+except ImportError:
+    print("Get it somewhere else")
+
+producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER)
+try:
+    data = {'imdb_id': sys.argv[1]}
+    producer.send('imdb_ids', json.dumps(data).encode())
+except IndexError:
+    print("Please provide a imdb_id")
