3b8d552281b00b18987382342ab7e76f0e0e6752
unruffled-nightingale <unruffled.nightingale@gmail.com>
2017-07-22 21:09:01 +0100

    Removed unnecessary sql code from project.

--- sql/views/v_companies.sql
+++
@@ -1,6 +0,0 @@
-create or replace view gather.v_companies as
-select y.name
-  from gather.tmdb_companies y
-  left join kino.companies x
-    on y.name = x.name
- where x.name is null
--- sql/views/v_company_roles.sql
+++
@@ -1,4 +0,0 @@
-create or replace view gather.v_company_roles as
-select 'Production'::text as role
- where 'Production' not in (select role
-                              from kino.company_roles)
