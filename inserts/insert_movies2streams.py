5f0e7469d9f9ab7808e02f1a0a0bf20a22fde337
tedjohansson <tedjohanssondeveloper@gmail.com>
2017-07-23 10:58:02 +0100

    Checking if the record is already in the table before inserting

--- inserts/insert_movies2streams.py
+++ inserts/insert_movies2streams.py
@@ -24,27 +24,34 @@ class InsertData(object):
         youtube_data = data['youtube_films_main']
 
         sql = """insert into kino.movies2streams (imdb_id, source, url, currency, price, format, purchase_type, tstamp)
-                 select x.imdb_id
-                      ,  unnest(array['GooglePlay', 'YouTube'])
-                      ,  unnest(array['https://play.google.com/store/movies/details?id=' || x.video_id,
-                                      'https://www.youtube.com/watch?v=' || x.video_id])
-                      , null::text
-                      , null::real
-                      , x.definition
-                      , 'rental'
-                      , CURRENT_DATE
-                  from json_to_recordset(%s) x (imdb_id varchar(1000), video_id varchar(1000), definition varchar(1000))
-                  union
-                 select imdb_id
-                      , 'iTunes'
-                      , url
-                      , '£'
-                          , unnest(array[rental_price, hd_rental_price, purchase_price, hd_purchase_price])
-                      , unnest(array['sd', 'hd', 'sd', 'hd'])
-                      , unnest(array['rental', 'rental', 'purchase', 'purchase'])
-                      , CURRENT_DATE
-                   from json_to_recordset(%s) y ( imdb_id varchar(1000), url varchar(1000), rental_price real
-                                                , hd_rental_price real, purchase_price real, hd_purchase_price real)
+                 select *
+                   from (select x.imdb_id
+                              ,  unnest(array['GooglePlay', 'YouTube']) as source
+                              ,  unnest(array['https://play.google.com/store/movies/details?id=' || x.video_id,
+                                              'https://www.youtube.com/watch?v=' || x.video_id]) as url
+                              , null::text
+                              , null::real
+                              , x.definition as format
+                              , 'rental' as purchase_type
+                              , CURRENT_DATE
+                           from json_to_recordset(%s) x (imdb_id varchar(1000), video_id varchar(1000), definition varchar(1000))
+                          union
+                         select imdb_id
+                              , 'iTunes' as source
+                              , url
+                              , '£'
+                              , unnest(array[rental_price, hd_rental_price, purchase_price, hd_purchase_price])
+                              , unnest(array['sd', 'hd', 'sd', 'hd']) as format
+                              , unnest(array['rental', 'rental', 'purchase', 'purchase']) as purchase_type
+                              , CURRENT_DATE
+                           from json_to_recordset(%s) y ( imdb_id varchar(1000), url varchar(1000), rental_price real
+                                                        , hd_rental_price real, purchase_price real, hd_purchase_price real)) foo
+                  where (foo.imdb_id, foo.source, foo.url, foo.format, foo.purchase_type) not in (select imdb_id
+                                                                                   , source
+                                                                                   , url
+                                                                                   , format
+                                                                                   , purchase_type
+                                                                                from kino.movies2streams)
                  """
 
         self.pg.pg_cur.execute(sql, (json.dumps(youtube_data), json.dumps(itunes_data)))
