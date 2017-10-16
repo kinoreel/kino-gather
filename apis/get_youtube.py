019f9167402780bcc398cbbdd149c668450d0dba
unruffled-nightingale <unruffled.nightingale@gmail.com>
2017-10-16 23:43:27 +0100

    Added tests for get_youtube. Partioned code into multiple classes

--- apis/get_youtube.py
+++ apis/get_youtube.py
@@ -7,54 +7,78 @@ try:
     YOUTUBE_FILMS_API = os.environ['API_KEY']
 except KeyError:
     try:
-        from GLOBALS import YOUTUBE_FILMS_API
+        from apis.GLOBALS import YOUTUBE_FILMS_API
     except ImportError:
         print("API is not known")
         exit()
 
+
 class GetAPI(object):
+
+    def __init__(self):
+        self.source_topic = 'itunes'
+        self.destination_topic = 'youtube'
+        self.get_data = RequestAPI().get_youtube
+        self.standardise_data = StandardiseResponse().standardise
+        self.choose_best = ChooseBest().get_best_match
+
+    def get_info(self, request):
+        imdb_id = request['imdb_id']
+        title = request['title']
+        runtime = request['runtime']
+        data = self.get_data(title)
+        if data:
+            data = self.standardise_data(imdb_id, data)
+            data = self.choose_best(data, title, runtime)
+            if data:
+                return data
+        else:
+            return None
+
+
+class RequestAPI(object):
+    """This class requests data for a given imdb_id from the YouTube API."""
 
     def __init__(self, api_key=YOUTUBE_FILMS_API):
         self.api_key = api_key
-        self.source_topic = 'itunes'
-        self.destination_topic = 'youtube'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         self.youtube = build('youtube', 'v3', developerKey=api_key)
-        self.empty_json = {}
 
-    def get_info(self, request):
-        '''
-        Thif function gets the movie info returned by the youtube_api
-        :param request: A json object containing at least imdb_id, runtime and movie title info.
-        :return: Film info returned by the youtube api.
-        '''
-        imdb_id = request['imdb_id']
-        title = request['omdb_main'][0]['title']
-        runtime = int(request['omdb_main'][0]['runtime'].replace(' min',''))
-        data = self.search_youtube(title)
-        # We check that we have successfully returned a result,
-        # else we return the empty json.
-        if data:
-            standardised_data = []
-            # We loop though the each response provided by the youtube api
-            # expanding and standardising the information.
-            for item in data:
-                standardised_data.append(self.get_movie_info(imdb_id, title, item))
-            # With our array of standardised information we then choose the film that
-            # best matches the film we requested.
-            youtube_main = self.get_best_film(standardised_data, title, runtime)
-        else:
-            youtube_main = self.empty_json
+    def get_youtube(self, title):
+        """
+        This function searches YouTube API for our requested title using the
+        function search_youtube. Then for each result, we request the content
+        and statistics information.
+        :param title: The film we are requesting from the YouTube API.
+        :return: A JSON object containing the response from the YouTube API.
+        """
+        youtube_data = []
+        response = self.search_youtube(title)
+        # For each film we in the youtube response, we
+        # request the content_details and statistics for the film, constructing
+        # a new dictionary of this information. We append these dictionaries
+        # to the array youtube_data.
+        for film in response:
+            video_id = film['id']['videoId']
+            info = film['snippet']
+            content_details = self.get_content_details(video_id)
+            get_stats = self.get_stats(video_id)
+            info.update(content_details)
+            info.update(get_stats)
+            info.update({'video_id': video_id})
+            youtube_data.append(info)
 
-        return {'youtube_films_main': youtube_main}
+        return youtube_data
 
     def search_youtube(self, title):
-        '''
+        """
         This function searches the youtube api for a specific film
         :param title: The film we are requesting from the youtube api.
-        :return: A json object containing the responses from youtube.
-        '''
+        :return: A JSON object containing the responses from youtube.
+        """
+        # We specify a number of parameters to ensure we
+        # only receive movies that are accessible from a british regional code.
         response = self.youtube.search().list(
             part='snippet',
             regionCode='gb',
@@ -65,118 +89,113 @@ class GetAPI(object):
         ).execute()
         return response['items']
 
-    def get_movie_info(self, imdb_id, title, data):
-        '''
-        This function cleans and pulls additional information for each film returned
-        by the initial response from the youtube api.
-        Ths function works in three stages.
-         - Standardise the response.
-         - Get and standardise information on video content
-         - Get and standardise information on video statistics
-        :param imdb_id: The original imdb_id
-        :param title: The original requested title
-        :param data: The json response for a sinlge entry returned by the youtube api.
-        :return: A standardised and extended json object containing information on the movie
-        '''
-        # Clean movie info
-        movie_info = self.fx_movie_item(data)
-        movie_info['imdb_id'] = imdb_id
-        movie_info['orig_title'] = title
-        video_id = movie_info['video_id']
-        # Get and fix content info
-        content = self.get_content_details(video_id)
-        content = self.fx_content_details(content)
-        movie_info.update(content)
-        # Get and fix stats info.
-        stats = self.get_stats(video_id)
-        stats = self.fx_stats(stats)
-        movie_info.update(stats)
-        return movie_info
-
-    def fx_movie_item(self, data):
-        movie = {}
-        movie['title'] = data['snippet']['title']
-        movie['description'] = data['snippet']['description']
-        movie['video_id'] = data['id']['videoId']
-        movie['channel_title'] = data['snippet']['channelTitle']
-        movie['channel_id'] = data['snippet']['channelId']
-        movie['published'] = data['snippet']['publishedAt']
-        return movie
-
     def get_content_details(self, video_id):
+        """
+        This function returns the content information - length, -
+        for a particular video.
+        :param video_id: The youtube video id.
+        :return: A JSON object containing the response from youtube.
+        """
         response = self.youtube.videos().list(
             part='contentDetails',
             id=video_id,
         ).execute()
-        return response
-
-    def fx_content_details(self, data):
-        content_details = {}
-        if data['items'][0]['contentDetails']['licensedContent']:
-            content_details['licenced'] = 'licenced'
-        else:
-            content_details['licenced'] = 'not licenced'
-
-        try:
-            content_details['region'] = ','.join(data['items'][0]['contentDetails']['regionRestriction']['allowed'])
-        except:
-            content_details['region'] = ''
-
-        content_details['dimension'] = data['items'][0]['contentDetails']['dimension']
-        content_details['caption'] = data['items'][0]['contentDetails']['caption']
-        content_details['duration'] = self.fix_runtime(data['items'][0]['contentDetails']['duration'])
-        content_details['definition'] = data['items'][0]['contentDetails']['definition']
-        return content_details
-
-    def fix_runtime(self, runtime):
-        '''
-        This function transforms the formatted time returned by youtube
-        into a minutes.
-        :param time: Time in the formatted by youtube - PT1H59M15S
-        :return: Duration in minutes
-        '''
-        runtime = re.sub('H|M', ':', runtime.replace('PT', '').replace('S', ''))
-        h, m, s = runtime.split(':')
-        return int(h)*60+int(m)
+        return response['items'][0]['contentDetails']
 
     def get_stats(self, video_id):
+        """
+        This function returns the stats information - likes, dislike, etc. -
+        for a particular video.
+        :param video_id: The youtube video id.
+        :return: A JSON object containing the response from youtube.
+        """
         response = self.youtube.videos().list(
             part='statistics',
             id=video_id
         ).execute()
-        return response
-
-    def fx_stats(self, data):
-        stats={}
-        stats['likes'] = data['items'][0]['statistics'].get('likeCount')
-        stats['dislikes'] = data['items'][0]['statistics'].get('dislikeCount')
-        stats['comments'] = data['items'][0]['statistics'].get('commentCount')
-        return stats
+        return response['items'][0]['statistics']
 
 
-    def get_best_film(self, data, requested_title, requested_runtime ):
-        '''
-        This function determines the film returned by the Youtube
+class StandardiseResponse(object):
+    """
+    This class reconstructs the response returned from the YouTube API, making
+    a new JSON object that is easier to handle for later applications.
+    """
+
+    def standardise(self, imdb_id, api_data):
+        """
+        We construct a new dictionary from teh API data, standardising the format
+        so we it can be handled easily in later applications.
+        :param imdb_id: The imdb_id for the film that was requested from OMDB API
+        :param api_data: The raw response from teh OMDB API
+        :return: A standardised dictionary.
+        """
+        main_data = []
+        for film in api_data:
+            standardised_film = self.get_main_data(imdb_id, film)
+            main_data.append(standardised_film)
+        return main_data
+
+    def get_main_data(self, imdb_id, api_data):
+        """
+        Gets the main data returned by the YouTube API, and constructs a dictionary
+        of this information. The api data is already pretty good, so we only have to change
+        a few small things.
+        :param imdb_id:
+        :param api_data:
+        :return:  A dictionary containing the main info for the film.
+        """
+        del api_data['licensedContent']
+        api_data['regionRestriction'] = ','.join(api_data['regionRestriction']['allowed'])
+        api_data['duration'] = self.fix_runtime(api_data['duration'])
+        api_data['imdb_id'] = imdb_id
+        return api_data
+
+    def fix_runtime(self, runtime):
+        """
+        This function transforms the formatted time returned by youtube
+        into a minutes.
+        :param time: Time in the formatted by youtube - PT1H59M15S
+        :return: Duration in minutes
+        """
+        runtime = re.sub('H|M', ':', runtime.replace('PT', '').replace('S', ''))
+        h, m, s = runtime.split(':')
+        return int(h) * 60 + int(m)
+
+class ChooseBest(object):
+    """
+    This class compares each of the films returned by the YouTube API against
+    the requested film and returns the best match.
+    """
+
+    def get_best_match(self, api_data, req_title, req_runtime):
+        """
+        This function determines the film returned by the YouTube
         API, that best matches the film requested.
         To do this we use compare the title and the runtime of the film.
         We then pull the best result based on this.
-        :param cleaned_movie_data: The list of dictionaries containing movie data
-        we want to compare
-        :param requested_title: The name of the title we requested
-        :param requested_runtime: The runtime of the
-        :return:
-        '''
-        match_scores = []
-        for i in data:
-            match_scores.append(self.get_match_score(i['title'], i['duration'], requested_title, requested_runtime))
-
+        :param api_data: The list of dictionaries containing movie data returned from teh YouTube API.
+        :param req_title: The name of the title we requested
+        :param req_runtime: The runtime of the
+        :return: The film with the best match score, or None if no match score is greater than 85.
+        """
+        match_scores = [self.get_match_score(e['title'], e['runtime'], req_title, req_runtime)
+                        for e in api_data]
         if max(match_scores) > 85:
-            index_of_best = match_scores.index(max(match_scores))
-            return data[index_of_best]
+            return api_data[match_scores.index(max(match_scores))]
         else:
-            return self.empty_json
+            return None
 
-    def get_match_score(self, title, runtime, requested_title, requested_runtime, ):
+    def get_match_score(self, title, runtime, requested_title, requested_runtime):
+        """
+        This returns a match score for a film returned by the YouTube API, constructed
+        from the title score minus the runtime score.
+        :param title: The title of the film returned by the YouTube API.
+        :param runtime: The runtime of the film returned by the YouTube API
+        :param requested_title: The title of the requested film.
+        :param requested_runtime: The runtime of the requested film.
+        :return: A score out of 100.
+        """
         title_score = self.get_title_score(title, requested_title)
         runtime_score = self.get_runtime_score(runtime, requested_runtime)
         match_score = title_score - runtime_score
@@ -194,12 +213,12 @@ class GetAPI(object):
         return title_score
 
     def get_runtime_score(self, runtime, requested_runtime):
-        '''
+        """
         This function provides a score for how closely the a file runtime
         matches the requested film runtime. Lower is better.
         :param title: The film runtime being compared.
         :param requested_title: The runtime was are comparing to.
         :return: An integer score
-        '''
+        """
         runtime_score = abs(runtime - requested_runtime)
         return runtime_score
--- apis/tests/test_get_youtube.py
+++ apis/tests/test_get_youtube.py
@@ -1,43 +1,213 @@
 import os
 import unittest
 
-from apis.get_youtube import GetAPI
+from apis.get_youtube import GetAPI, RequestAPI, ChooseBest, StandardiseResponse
 
-try:
-    YOUTUBE_FILMS_API = os.environ['API_KEY']
-except KeyError:
-    try:
-        import GLOBALS
-    except ImportError:
-        print("API is not known")
-        exit()
 
-class TestIGetAPI(unittest.TestCase):
+class TestGetAPI(unittest.TestCase):
+    """Testing RequestAPI"""
+    @classmethod
+    def setUpClass(cls):
+        cls.get = GetAPI()
+
+
+class TestRequestAPI(unittest.TestCase):
+    """Testing RequestAPI"""
 
     @classmethod
     def setUpClass(cls):
-        cls.api = GetAPI(GLOBALS.YOUTUBE_FILMS_API)
+        cls.req = RequestAPI()
 
-    def test_get_info(self):
-        request = {'omdb_main': [{'title': 'Trainspotting', 'runtime': '94'}],
-                   'imdb_id': 'tt0117951'
-                   }
-        best_film = self.api.get_info(request)
+    def test_get_youtube(self):
+        # Blade Runner
+        title = 'Blade Runner'
+        response = self.req.get_youtube(title)
+
+    def test_search_youtube(self):
+        # Blade Runner
+        title = 'Blade Runner'
+        response = self.req.search_youtube(title)
+
+    def test_get_content_details(self):
+        # Blade Runner
+        video_id = '59cQqLrdmK8'
+        response = self.req.get_content_details(video_id)
+
+    def test_get_stats(self):
+        # Blade Runner
+        video_id = '59cQqLrdmK8'
+        response = self.req.get_stats(video_id)
+
+class TestStandardiseResponse(unittest.TestCase):
+    """Testing the class StandardiseResponse"""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.stan = StandardiseResponse()
+        cls.response = [{
+            'commentCount': '8',
+            'likeCount': '223',
+            'definition': 'hd',
+            'video_id': '59cQqLrdmK8',
+            'projection': 'rectangular',
+            'publishedAt': '2013-07-19T04:02:19.000Z',
+            'licensedContent': True,
+            'title': 'Blade Runner: The Final Cut Special Edition',
+            'caption': 'true',
+            'duration': 'PT1H57M29S',
+            'channelTitle': 'warnervoduk',
+            'regionRestriction': {
+                'allowed': ['IE', 'GB']
+            },
+            'dislikeCount': '55',
+            'channelId': 'UCsDKdkvGBqaD-KINQP8WAEA',
+            'dimension': '2d',
+            'liveBroadcastContent': 'none',
+            'description': "The one that started it all. Ridley Scott's Blade Runner is one of the most important science-fiction movies of the 20th Century. Its futuristic depiction of a ...",
+            'favoriteCount': '0'
+        }, {
+            'likeCount': '43',
+            'definition': 'hd',
+            'video_id': 'rJ-T1ddFVRw',
+            'projection': 'rectangular',
+            'publishedAt': '2014-01-20T09:56:51.000Z',
+            'licensedContent': True,
+            'title': 'Blade Runner',
+            'caption': 'true',
+            'duration': 'PT1H57M9S',
+            'channelTitle': 'warnervoduk',
+            'regionRestriction': {
+                'allowed': ['GB', 'IE']
+            },
+            'dislikeCount': '25',
+            'channelId': 'UCsDKdkvGBqaD-KINQP8WAEA',
+            'dimension': '2d',
+            'liveBroadcastContent': 'none',
+            'description': '21st-century detective Rick Deckard brings his masculine-yet-vulnerable presence to this stylish noir thriller. In a future of high-tech possibility soured by urban ...',
+            'favoriteCount': '0'
+        }]
+
+    def test_get_main_data(self):
+        main_data = self.stan.get_main_data(self.response[0])
+        expected_result = {
+            'dislikeCount': '55',
+            'title': 'Blade Runner: The Final Cut Special Edition',
+            'channelTitle': 'warnervoduk',
+            'projection': 'rectangular',
+            'caption': 'true',
+            'dimension': '2d',
+            'commentCount': '8',
+            'definition': 'hd',
+            'video_id': '59cQqLrdmK8',
+            'regionRestriction': 'IE,GB',
+            'description': "The one that started it all. Ridley Scott's Blade Runner is one of the most important science-fiction movies of the 20th Century. Its futuristic depiction of a ...",
+            'duration': 117,
+            'channelId': 'UCsDKdkvGBqaD-KINQP8WAEA',
+            'likeCount': '223',
+            'favoriteCount': '0',
+            'liveBroadcastContent': 'none',
+            'publishedAt': '2013-07-19T04:02:19.000Z'
+        }
+        self.assertEqual(main_data, expected_result)
+
+    def test_standardised(self):
+        main_data = self.stan.standardise(self.response)
+        expected_result = [{
+            'dimension': '2d',
+            'caption': 'true',
+            'video_id': '59cQqLrdmK8',
+            'projection': 'rectangular',
+            'commentCount': '8',
+            'publishedAt': '2013-07-19T04:02:19.000Z',
+            'title': 'Blade Runner: The Final Cut Special Edition',
+            'dislikeCount': '55',
+            'duration': 117,
+            'channelId': 'UCsDKdkvGBqaD-KINQP8WAEA',
+            'regionRestriction': 'IE,GB',
+            'channelTitle': 'warnervoduk',
+            'favoriteCount': '0',
+            'liveBroadcastContent': 'none',
+            'description': "The one that started it all. Ridley Scott's Blade Runner is one of the most important science-fiction movies of the 20th Century. Its futuristic depiction of a ...",
+            'likeCount': '223',
+            'definition': 'hd'
+        }, {
+            'dimension': '2d',
+            'caption': 'true',
+            'video_id': 'rJ-T1ddFVRw',
+            'projection': 'rectangular',
+            'liveBroadcastContent': 'none',
+            'publishedAt': '2014-01-20T09:56:51.000Z',
+            'title': 'Blade Runner',
+            'dislikeCount': '25',
+            'duration': 117,
+            'channelId': 'UCsDKdkvGBqaD-KINQP8WAEA',
+            'regionRestriction': 'GB,IE',
+            'channelTitle': 'warnervoduk',
+            'favoriteCount': '0',
+            'description': '21st-century detective Rick Deckard brings his masculine-yet-vulnerable presence to this stylish noir thriller. In a future of high-tech possibility soured by urban ...',
+            'likeCount': '43',
+            'definition': 'hd'
+        }]
+        self.assertEqual(main_data, expected_result)
+
+
+class TestChooseBest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.cb = ChooseBest()
+        cls.req = RequestAPI()
+
+    def test_get_title_score(self):
+        score = self.cb.get_title_score('Blade Runner', 'Blade Runner')
+        self.assertEqual(score, 100)
+        score = self.cb.get_title_score('Blade Runner', 'BLADE RUNNER')
+        self.assertEqual(score, 100)
+        score = self.cb.get_title_score('Blade Runner', 'Blade Runner 2049')
+        self.assertEqual(score, 83)
+        score = self.cb.get_title_score('Jaws', 'Jaws 2')
+        self.assertEqual(score, 80)
+        score = self.cb.get_title_score("Bill and Ted's Excellent Adventure", "Bill and Ted's Bogus Journey")
+        self.assertEqual(score, 61)
+
+    def test_get_runtime_score(self):
+        score = self.cb.get_runtime_score(100, 200)
+        self.assertEqual(score, 100)
+        score = self.cb.get_runtime_score(200, 100)
+        self.assertEqual(score, 100)
+
+    def test_get_match_score(self):
+        score = self.cb.get_match_score('Blade Runner', 117, 'Blade Runner', 117)
+        self.assertEqual(score, 100)
+        score = self.cb.get_match_score('Blade Runner', 117, 'Blade Runner 2049', 164)
+        self.assertEqual(score, 36)
+        score = self.cb.get_match_score('Jaws', 124, 'Jaws 2', 116 )
+        self.assertEqual(score, 72)
+
+    def test_get_best_match(self):
+        """
+        We request a number of films from the YouTube API using the RequestAPI class.
+        We then run the function  function and compare the result
+        :return:
+        """
+        title = 'Blade Runner'
+        runtime = '117'
+        data = self.req.get_youtube(title)
+        best_film = self.cb.get_best_match(data,title, runtime)
         self.assertEqual(best_film['youtube_films_main']['title'], 'Trainspotting')
 
         request = {'omdb_main': [{'title': 'El Club', 'runtime': '98'}],
                    'imdb_id': 'tt4375438'
                    }
 
-        best_film = self.api.get_info(request)
+        best_film = self.req.get_youtube(request)
         self.assertEqual(best_film['youtube_films_main'], {})
 
         request = {'omdb_main': [{'title': 'Planet of the Apes', 'runtime': '112'}],
                    'imdb_id': 'tt0063442'
                    }
 
-        best_film = self.api.get_info(request)
-        print(best_film)
+        best_film = self.req.get_youtube(request)
         self.assertEqual(best_film['youtube_films_main']['title'], 'Planet of the Apes')
 
 
